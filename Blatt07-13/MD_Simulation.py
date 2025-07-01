2708aefbf827fa96663f4dcad2435e801fc8d8e7
Sebastian Utz <sebastian.utz@uni-ulm.de>
2025-07-01 18:33:14 +0200

    Struktur erneuert

--- Blatt07-13/MD_Simulation.py
+++ Blatt07-13/MD_Simulation.py
@@ -28,7 +28,7 @@ path_to_Li = 'Li/POSCAR_4x4x3_20vac'
 path_to_Mg = 'Mg/POSCAR_4x4x3_20vac'
 path_to_MgBulk = 'Mg/POSCAR_3x3x3Bulk'
 path_to_LiPSCl = 'LiPSCl/POSCAR_2x2x2'
-path_to_MgScSe =  'MgSc2Se4/POSCAR'
+path_to_MgScSe =  'MgSc2Se4/POSCAR_defect'
 
 structure_Li = read(path_to_Li,format='vasp')
 structure_Mg = read(path_to_Mg,format='vasp')
@@ -141,7 +141,7 @@ mace_calc_bulk = MACECalculator(
 MgScSe_relaxed = relaxation_mace(structure_MgScSe, mace_calc, fix_cell = False, f = 0.01)
 
 T_list = [800.0,1200.0,1600.0,2000.0]
-traj_dir = 'MgSc2Se4_Trajectories_3fs'
+traj_dir = 'MgSc2Se4Defect_Trajectories_3fs'
 for Temp in T_list:
     print(f'Temperatur ist {Temp}')
     structure_tempo = copy.deepcopy(MgScSe_relaxed['relaxed_structure'])
